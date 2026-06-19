6cb603895facd9c6de094d983fb616c54cfbba67
Jing Cao <jingcaomath@gmail.com>
2026-06-18 23:55:22 -0400

    tavily crawling

--- ingestion.py
+++ ingestion.py
@@ -41,7 +41,25 @@ tavily_crawl = TavilyCrawl()
 
 
 async def main():
-    """Main function to run the ingestion process."""
+    """Main async function to orchestrate the entire process."""
+    log_header("DOCUMENTATION INGESTION PIPELINE")
+
+    log_info(
+        "TavilyCrawl: Starting to crawl documentation from https://python.langchain.com/",
+        Colors.PURPLE,
+        )
+    
+    # Crawl the documentation site
+    res = tavily_crawl.invoke({
+        "url": "https://python.langchain.com/",
+        "max_depth": 1,
+        "extract_depth": "advanced",
+        #"instructions": "content on AI agents"
+    })
+
+    all_docs = [Document(page_content=result["raw_content"], metadata={"source": result["url"]}) 
+                for result in res["results"]]
+    log_success(f"TavilyCrawl: Successfully crawled {len(all_docs)} documents from the site.")
 
 if __name__ == "__main__":
     asyncio.run(main()) 
